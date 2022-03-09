d7d31e7a343671b2de05793e8e9ef4ad7f425b69
Harshad Vilekar <harshad.vilekar@oracle.com>
2022-03-09 11:29:16 -0800

    created service paramters template file

--- src/cicd.sh
+++ src/cicd.sh
@@ -1,4 +1,6 @@
 # full path to ssh private key file. This file is created here.
 DIR=`dirname $0`
 source $DIR/service_params.sh
-python $DIR/cicd.py $BASTION_PUBLIC_IP $ADMIN_PRIVATE_IP $JENKINS_URL $SSH_PRIVATE_KEY_FILE
+CMD="python $DIR/cicd.py $BASTION_PUBLIC_IP $ADMIN_PRIVATE_IP $JENKINS_URL $SSH_KEY_FILE"
+echo $CMD
+$CMD
--- src/service_params.sh
+++ src/service_params.sh
@@ -1,13 +1,7 @@
-# Update these values from "terraform output" of your service
-# To get Jenkins URL IP, run following command on service admin VM:
-# [opc@<mu-service>-admin ~]$ kubectl get svc -n wlsoke-ingress-nginx -l app.kubernetes.io/name=ingress-nginx -o jsonpath='{.items[*].status.loadBalancer.ingress[0].ip}'
-export BASTION_PUBLIC_IP=129.153.192.188
-export ADMIN_PRIVATE_IP=10.0.2.4
-export JENKINS_URL=http://10.0.6.9/jenkins
-
 DIR=`dirname $0`
-# WLS_OKE_SERVICE_SECRETS_FILE
-source "DIR/../secrets/wlsoke_service_secrets.sh"
-export SSH_PRIVATE_KEY_FILE=$DIR/ssh_key
-echo "$SSH_PRIVATE_KEY" > $SSH_PRIVATE_KEY_FILE
-chmod 600 $SSH_PRIVATE_KEY_FILE
+# This file is copied over by the CI/CD job, and sets following parameters:
+# BASTION_PUBLIC_IP, ADMIN_PRIVATE_IP, JENKINS_URL, JENKINS_CREDS, SSH_KEY
+source "$DIR/wlsoke_service_parameters.sh"
+export SSH_KEY_FILE=$DIR/ssh_key
+echo "$SSH_KEY" > $SSH_KEY_FILE
+chmod 600 $SSH_KEY_FILE
